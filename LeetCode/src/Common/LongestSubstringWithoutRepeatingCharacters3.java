c3ca4c3edd109ab6d2ab833da8cf83769836bc93
王金星 <wangjinxing@renrenche.com>
2017-08-08 12:09:58 +0800

    多指针

--- LeetCode/src/Common/LongestSubstringWithoutRepeatingCharacters3.java
+++ LeetCode/src/Common/LongestSubstringWithoutRepeatingCharacters3.java
@@ -1,13 +1,15 @@
 package Common;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
  * Created by oukinsei on 17/8/7.
  */
 public class LongestSubstringWithoutRepeatingCharacters3 {
-    public int lengthOfLongestSubstring(String s) {
+    public int lengthOfLongestSubstringSet(String s) {
         if(null == s) return 0;
         int result = 0, start = 0;
         Set<Character> charSet = new HashSet<>();
@@ -27,6 +29,19 @@ public class LongestSubstringWithoutRepeatingCharacters3 {
         }
         //last compare
         return maxInt(result, charSet.size());
+    }
+    public int lengthOfLongestSubstring(String s) {
+        if(null == s) return 0;
+        int result = 0, start = -1;
+        Map<Character,Integer> charMap = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            if(charMap.containsKey(s.charAt(i))){
+                start = maxInt(start, charMap.get(s.charAt(i)));
+            }
+            charMap.put(s.charAt(i), i);
+            result = maxInt(result, i - start);
+        }
+        return result;
     }
     private int maxInt(int a, int b){
         return a > b ? a : b;
